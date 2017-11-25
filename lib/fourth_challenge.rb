edcdb29817bc3634a51d9cf07a606b6c1448cd63
Chris Gullickson <cgullickson5@gmail.com>
2017-11-25 00:22:13 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,4 +1,5 @@
-epic_tragedy = {
+def fourht_challenge
+  epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -18,3 +19,4 @@ epic_tragedy = {
       ]
    }
 }
+end
